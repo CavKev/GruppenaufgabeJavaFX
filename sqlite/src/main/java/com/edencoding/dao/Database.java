5965e0fcd40cd58f89cda6b5842cfadaef1d4880
Kevin Kretz <kevin.kretz.2003@gmail.com>
2026-05-26 11:10:25 +0200

    First Configuration Commit

---
+++ sqlite/src/main/java/com/edencoding/dao/Database.java
@@ -0,0 +1,61 @@
+package com.edencoding.dao;
+
+import com.edencoding.SQLiteExampleApp;
+
+import java.sql.*;
+import java.time.LocalDateTime;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class Database {
+
+    /**
+     * Location of database
+     */
+    private static final String location = SQLiteExampleApp.class.getResource("database/Students.db").toExternalForm();
+
+
+    public static boolean isOK() {
+        if (!checkDrivers()) return false; //driver errors
+
+        if (!checkConnection()) return false; //can't connect to db
+
+        return true;
+    }
+
+    private static boolean checkDrivers() {
+        try {
+            Class.forName("org.sqlite.JDBC");
+            DriverManager.registerDriver(new org.sqlite.JDBC());
+            return true;
+        } catch (ClassNotFoundException | SQLException classNotFoundException) {
+            Logger.getAnonymousLogger().log(Level.SEVERE, LocalDateTime.now() + ": Could not start SQLite Drivers");
+            return false;
+        }
+    }
+
+    private static boolean checkConnection() {
+        try (Connection connection = connect()) {
+            return connection != null;
+        } catch (SQLException e) {
+            Logger.getAnonymousLogger().log(Level.SEVERE, LocalDateTime.now() + ": Could not connect to database");
+            return false;
+        }
+    }
+
+    protected static Connection connect() {
+        String dbPrefix = "jdbc:sqlite:";
+        Connection connection;
+        try {
+            connection = DriverManager.getConnection(dbPrefix + location);
+        } catch (SQLException exception) {
+            Logger.getAnonymousLogger().log(Level.SEVERE,
+                    LocalDateTime.now() + ": Could not connect to SQLite DB at " +
+                            location);
+            return null;
+        }
+        return connection;
+    }
+
+
+}
